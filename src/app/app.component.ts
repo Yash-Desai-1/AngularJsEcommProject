b47f45e0b684f2a0955fd3f6afe359d042dd37c5
Yash-Desai-1 <cm.a.26yash.desai@gmail.com>
2025-02-02 20:16:27 +0530

    created a product model -> product list -> product card -> cart service -> injected in product lits and product card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Components/header/header.component";
+import { ProductListComponent } from "./pages/product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ProductListComponent],
   template: `
     <app-header/>
+    <app-product-list/>
   `,
   styles: [],
 })
---
+++ src/app/pages/product-list/product-card/product-card.component.ts
@@ -0,0 +1,48 @@
+import { Component ,inject,input} from '@angular/core';
+import { Product } from '../../../models/product.model';
+import { PrimaryButtonComponent } from "../../../Components/primary-button/primary-button.component";
+import { CartService } from '../../../services/cart.service';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [PrimaryButtonComponent],
+  template: `
+   <div
+      class="bg-white shadow-md border rounded-xl p-6 flex flex-col gap-6 relative"
+    >
+      <div class="mx-auto">
+        <img
+          [src]="product().image"
+          class="w-[200px] h-[100px] object-contain"
+        />
+      </div>
+      <div class="flex flex-col">
+        <span class="text-md font-bold">{{ product().title }}</span>
+        <span class="text-sm"> {{ '$' + product().price }}</span>
+        <app-primary-button
+          
+          class="mt-3"
+          label="Add to Cart"
+          (btnClicked)="cartService.addToCart(product())" 
+          
+        />
+      </div>
+
+      <span
+        class="absolute top-2 right-3 text-sm font-bold"
+        [class]="product().stock ? 'text-green-500' : 'text-red-500'"
+      >
+        @if (product().stock) {
+        {{ product().stock }} left } @else { Out of stock }
+      </span>
+    </div>
+  `,
+  styles: `
+  `
+})
+export class ProductCardComponent {
+   product= input.required<Product>();
+
+   cartService=inject(CartService);
+}
+ 
