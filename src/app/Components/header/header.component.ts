9ef77f0f18250dc7ec7baadad41091507e46d546
Yash-Desai-1 <cm.a.26yash.desai@gmail.com>
2025-02-02 18:35:40 +0530

    created a dynamic button using input/output

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-100 px-4 shadow-md py-3">
-      <span>My Store</span>
+    <div class="bg-slate-100 px-4 shadow-md py-3 flex justify-between items-center">
+      <span class="text-xl">My Store</span>
+      <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()"/>
     </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
+  // dynamic value for input button
+   buttonName: string="Add to Cart";
+   cart =signal("Cart");
+  // output event
+  showButtonClicked() {
+    console.log('button was clicked');
+}
 
 
 }
